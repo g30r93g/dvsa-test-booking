9fd58d9e1ffae609c4f20a850cf1738ff889fb7e
George Nick Gorzynski <georgegorzynski@me.com>
2025-06-26 23:03:43 +0100

    Scaffold Routing

---
+++ app/(frontend)/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+export default async function DashboardPage() {
+    // Todo: Fetch the user's booking data
+
+    return (
+        <div>
+            <h1>Your Bookings</h1>
+        </div>
+    )
+}
