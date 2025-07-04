e7fc0e5089fb84aabc6ba05a05296ddeda6d2fa6
George Nick Gorzynski <georgegorzynski@me.com>
2025-07-04 20:49:04 +0100

    Update `instructor_students` db schema

--- db/schema/instructor-students.ts
+++ db/schema/instructor-students.ts
@@ -1,9 +1,20 @@
-import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
 import { instructor } from "./instructors";
 import { learner } from "./learners";
+
+export const linkStateEnum = pgEnum("link_state", [
+    "pending",
+    "cancelled",
+    "accepted",
+    "rejected",
+    "removed",
+]);
 
 export const instructorStudent = pgTable("instructor_students", {
     id: uuid("id").primaryKey().defaultRandom(),
     instructorId: uuid("instructor_id").notNull().references(() => instructor.id),
     studentId: uuid("student_id").notNull().references(() => learner.id),
+    linkState: linkStateEnum("link_state").notNull().default("pending"),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
